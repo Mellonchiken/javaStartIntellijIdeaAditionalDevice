6fe211b4db27f113edb9f25bc515e180592071ee
Melnychenko Maksim <melnychenko.maks@gmail.com>
2024-04-24 16:40:12 +0200

    Commited changes

---
+++ src/sk/com/ymca/javaStart/lecture1/classWork/Translator.java
@@ -0,0 +1,68 @@
+package sk.com.ymca.javaStart.lecture1.classWork;
+
+import java.util.Scanner;
+
+public class Translator {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Do you want to use this program? 1 - 'Yes' 2 - 'No'");
+            int choice = scan.nextInt();
+
+            if (choice == 1) {
+                System.out.println("Enter ukrainian word");
+                String word = scan.next();
+
+                switch (word) {
+                    case "книга": {
+                        System.out.println("book");
+                        break;
+                    }
+                    case "чашка": {
+                        System.out.println("cup");
+                        break;
+                    }
+                    case "коробка": {
+                        System.out.println("box");
+                        break;
+                    }
+                    case "куртка": {
+                        System.out.println("jacket");
+                        break;
+                    }
+                    case "серце": {
+                        System.out.println("heart");
+                        break;
+                    }
+                    case "крапка": {
+                        System.out.println("dot");
+                        break;
+                    }
+                    case "стілець": {
+                        System.out.println("chair");
+                        break;
+                    }
+                    case "літера": {
+                        System.out.println("letter");
+                        break;
+                    }
+                    case "сніданок": {
+                        System.out.println("breakfast");
+                        break;
+                    }
+                    case "вікно": {
+                        System.out.println("window");
+                        break;
+                    }
+                    default: {
+                        System.out.println("i don`t know this word");
+                    }
+                }
+                break;
+            } else if (choice == 2) {
+                break;
+            }
+        }
+    }
+}
